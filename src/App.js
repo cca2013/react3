a4661cbc398b8af4008c146b59cc986b59d03d36
unknown <skafosbiz@gmail.com>
2021-01-08 23:43:08 +0200

    'myfirstreact'

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload Konstantinos 
+        </p>Anastasiadis Konstantinos
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+		<a
+          className="App-link"
+          href="https://skafosbiz.wixsite.com/eshop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Eshop example
+        </a>
+				<a
+          className="App-link"
+          href="https://000575423.codepen.website/test.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Codepen example
+        </a>
+		
+      </header>
+    </div>
+  );
+}
+
+export default App;
